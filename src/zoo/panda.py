9a139843d27368dc56dfa10e7a778eaf0dea98b0
RufaidaK <124273686+RufaidaK@users.noreply.github.com>
2026-01-15 15:05:20 +0000

    panda

--- src/zoo/panda.py
+++ src/zoo/panda.py
@@ -1,12 +1,17 @@
 from .animal import Animal
 
 class Panda(Animal):
-    def_init_(self, name= "Sally")
-    super().__init__(name, species="Panda")
+    def __init__(self, name= "Sally"):
+      super().__init__(name, species="Panda")
 
     def sound(self):
-        return ""
+        return "Hellooo"
 
     def action(self):
         return "sleeps all day"
-    
+
+    def test_panda():
+        assert self.name() == "Sally"
+        assert self.species() == "Panda"
+        assert self.sound() == "Hellooo"
+        assert self.action() == "sleeps all day"
